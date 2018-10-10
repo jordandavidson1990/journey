2f38265de6dec15d249dec25ce5f14edab5d57e2
Jordan Davidson <jordandavidson1990@hotmail.com>
2018-10-10 16:20:59 +0100

    9 pass

---
+++ models/traveller.js
@@ -0,0 +1,38 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((start) => start.startLocation)};
+
+
+Traveller.prototype.getJourneyEndLocations = function() {
+ return this.journeys.map((end) => end.endLocation)};
+
+Traveller.prototype.getModesOfTransport = function() {
+return this.journeys.map((x) => x.transport)
+};
+
+Traveller.prototype.getJourneysByTransport = function(transport) {
+  // const result = this.journeys.filter((journey)=>{
+  //   return journey.transport === transport
+  // });
+  // return result
+  // }
+  return this.journeys.filter((journey)=> journey.transport === transport)};
+
+
+Traveller.prototype.getJourneysByMinDistance = function(minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function() {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function() {
+
+};
+
+
+module.exports = Traveller;
